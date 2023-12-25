3cf3e35821f43dcaed1be6674b38ea16f47d2748
yjh1008 <exesea12@gmail.com>
2023-12-26 00:02:05 +0900

    상품 css 적용

--- nuxt-learn/pages/index.vue
+++ nuxt-learn/pages/index.vue
@@ -1,6 +1,75 @@
 <template>
-  <div>
-    여기가 index 페이지입니다.
-    <NuxtLink to="/main">메인페이지 이동</NuxtLink>
+  <div clas="app">
+    <SearchInput/>
+    <maiin>
+      <ul>
+      <li v-for="p in data" :key="p.id" class="item flex">
+        <img :src="p.imageUrl" :alt="p.name" class="product-image"/>
+        <p>{{ p.name }}</p>
+        <span>{{ p.price }}</span>
+      </li>
+    </ul>
+    <div class="cart-wrapper">
+      <button class="btn" @click="routeToCartPage">장바구니 바로가기</button>
+    </div>
+    </maiin>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useAsyncData } from 'nuxt/app';
+import SearchInput from '~/components/SearchInput.vue';
+interface Product {
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: string;
+};
+
+const { data , pending } = await useAsyncData<Product[]>(
+  'products',
+  async () => { 
+    let ret:Product[] = await $fetch('http://localhost:3000/products');
+    ret.forEach((v) => v.imageUrl = `https://picsum.photos/id/${v.id}/640/480`);
+    return ret;
+  }
+)
+const routeToCartPage = () => {
+
+}
+
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
